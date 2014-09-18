da40b0499d89a62238929c5bfe13ea1b2ad0675d
Jeffrey D. Gordon <jdg@gauthic.com>
2014-09-18 07:52:46 -0500

    Experimenting with global backup and restore

--- js/src/localstorage.js
+++ js/src/localstorage.js
@@ -69,3 +69,113 @@ function get_localstorage_list() {
 		);
 	}
 }
+
+function get_backup_summary() {
+	html = "";
+
+	for (var key in localStorage){
+    	if (localStorage.hasOwnProperty(key)) {
+    		counted_items = 0;
+    		if(
+    			key.indexOf("current_") == -1
+    			&& key.indexOf("gm_control_") == -1
+    			&& key != "localStats"
+    			&& key != "saved_printcarts"
+    			&& key != "extras_search_term"
+    		) {
+    			if(localStorage[key] && localStorage[key].length >  0) {
+    				try{
+    					html += "" + uc_words(key) + " has " + JSON.parse(localStorage[key]).length + " items<br />";
+    					counted_items += JSON.parse(localStorage[key]).length;
+    				}
+    				catch(err) {
+    					html += "" + uc_words(key) + " has an encoding error.<br />";
+    				}
+
+    			} else {
+    	     		html += "" + uc_words(key) + " is empty<br />";
+    			}
+    		}
+   	 	}
+
+	}
+
+	if(counted_items > 0)
+		html += "<br /><button class=\"btn btn-primary js-export-data\">Download Data</button>";
+
+	return html;
+}
+function export_as_file() {
+
+	var export_as_file_object = [];
+	var export_count = 0;
+
+	for (var key in localStorage){
+    	if (localStorage.hasOwnProperty(key)) {
+    		if(
+    			key.indexOf("current_") == -1
+    			&& key.indexOf("gm_control_") == -1
+    			&& key != "localStats"
+    			&& key != "saved_printcarts"
+    			&& key != "extras_search_term"
+    		) {
+    			export_as_file_object.push( {
+    				name: key,
+    				data: JSON.parse(localStorage[key])
+    			} );
+    			export_count += JSON.parse(localStorage[key]).length;
+    		}
+    	}
+    }
+
+    if(export_count > 0) {
+    //	console.log ( export_as_file_object["characters"] );
+    //	console.log( JSON.stringify(export_as_file_object["characters"]) );
+    	var export_data = JSON.stringify(export_as_file_object);
+
+		var blob = new Blob([export_data], {type: "text/plain;charset=utf-8"});
+		saveAs(blob, "savage worlds web tools export.txt");
+    }
+
+}
+
+function import_to_localstorage( import_object ) {
+	for( local_counter = 0; local_counter < import_object.length; local_counter++ ) {
+		if( import_object[local_counter].name &&  import_object[local_counter].data ) {
+			//console.log( "Concating localStorage[ " + import_object[local_counter].name +" ]");
+//			console.log( import_object[local_counter].data );
+
+			if( localStorage[ import_object[local_counter].name ] )
+				current_data = JSON.parse(localStorage[ import_object[local_counter].name ]);
+			else
+				current_data = Array();
+
+			current_data = current_data.concat( import_object[local_counter].data );
+
+			localStorage[ import_object[local_counter].name ] = JSON.stringify(current_data);
+		}
+	}
+}
+
+function import_restore_file( import_data, run_import ) {
+	try {
+
+		import_data = import_data.substr( import_data.indexOf(",") + 1 );
+		import_data = window.atob( import_data );
+
+		import_object = JSON.parse( import_data );
+	//	console.log(import_object);
+		if( !import_object[0].name ) {
+			return false;
+		} else {
+			if( run_import ) {
+				import_to_localstorage( import_object );
+			}
+			return true;
+		}
+	}
+	catch(error){
+		//console.log("Not a JSON");
+		return false;
+	}
+}
